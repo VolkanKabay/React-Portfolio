5896a60998d64225000200a175023b7d23e5e168
VolkanKabay <124468930+VolkanKabay@users.noreply.github.com>
2024-07-19 01:00:07 +0200

    disclaimer

---
+++ src/Disclaimer.tsx
@@ -0,0 +1,105 @@
+import { Box, Typography, Link } from "@mui/material";
+
+const Disclaimer = () => {
+  return (
+    <Box className="impressum" p={3}>
+      <Typography variant="h2" gutterBottom>
+        Impressum
+      </Typography>
+      <Typography variant="body1" paragraph>
+        Angaben gemäß § 5 DDG
+      </Typography>
+      <Typography variant="body1" paragraph>
+        {import.meta.env.VITE_ADRESS_NAME}
+        <br />
+        {import.meta.env.VITE_ADRESS_STREET}
+        <br />
+        {import.meta.env.VITE_ADRESS_CITY}
+      </Typography>
+      <Typography variant="body1" paragraph>
+        <strong>Vertreten durch:</strong> <br />
+        Volkan Kabay <br />
+      </Typography>
+      <Typography variant="body1" paragraph>
+        <strong>Kontakt:</strong> <br />
+        E-Mail:
+        <Link
+          sx={{
+            textDecoration: "none",
+            color: "inherit",
+            marginLeft: "0.5rem",
+          }}
+          href="mailto:volkankabay@gmail.com"
+        >
+          volkankabay@gmail.com
+        </Link>
+      </Typography>
+      <Typography variant="body1" paragraph>
+        <strong>Haftungsausschluss:</strong> <br />
+        <strong>Haftung für Inhalte</strong> <br />
+        Die Inhalte unserer Seiten wurden mit größter Sorgfalt erstellt. Für die
+        Richtigkeit, Vollständigkeit und Aktualität der Inhalte können wir
+        jedoch keine Gewähr übernehmen. Als Diensteanbieter sind wir gemäß § 7
+        Abs.1 DDG für eigene Inhalte auf diesen Seiten nach den allgemeinen
+        Gesetzen verantwortlich. Nach §§ 8 bis 10 DDG sind wir als
+        Diensteanbieter jedoch nicht verpflichtet, übermittelte oder
+        gespeicherte fremde Informationen zu überwachen oder nach Umständen zu
+        forschen, die auf eine rechtswidrige Tätigkeit hinweisen.
+        Verpflichtungen zur Entfernung oder Sperrung der Nutzung von
+        Informationen nach den allgemeinen Gesetzen bleiben hiervon unberührt.
+        Eine diesbezügliche Haftung ist jedoch erst ab dem Zeitpunkt der
+        Kenntnis einer konkreten Rechtsverletzung möglich. Bei Bekanntwerden von
+        entsprechenden Rechtsverletzungen werden wir diese Inhalte umgehend
+        entfernen.
+      </Typography>
+      <Typography variant="body1" paragraph>
+        <strong>Haftung für Links</strong> <br />
+        Unser Angebot enthält Links zu externen Webseiten Dritter, auf deren
+        Inhalte wir keinen Einfluss haben. Deshalb können wir für diese fremden
+        Inhalte auch keine Gewähr übernehmen. Für die Inhalte der verlinkten
+        Seiten ist stets der jeweilige Anbieter oder Betreiber der Seiten
+        verantwortlich. Die verlinkten Seiten wurden zum Zeitpunkt der
+        Verlinkung auf mögliche Rechtsverstöße überprüft. Rechtswidrige Inhalte
+        waren zum Zeitpunkt der Verlinkung nicht erkennbar. Eine permanente
+        inhaltliche Kontrolle der verlinkten Seiten ist jedoch ohne konkrete
+        Anhaltspunkte einer Rechtsverletzung nicht zumutbar. Bei Bekanntwerden
+        von Rechtsverletzungen werden wir derartige Links umgehend entfernen.
+      </Typography>
+      <Typography variant="body1" paragraph>
+        <strong>Urheberrecht</strong> <br />
+        Die durch die Seitenbetreiber erstellten Inhalte und Werke auf diesen
+        Seiten unterliegen dem deutschen Urheberrecht. Die Vervielfältigung,
+        Bearbeitung, Verbreitung und jede Art der Verwertung außerhalb der
+        Grenzen des Urheberrechtes bedürfen der schriftlichen Zustimmung des
+        jeweiligen Autors bzw. Erstellers. Downloads und Kopien dieser Seite
+        sind nur für den privaten, nicht kommerziellen Gebrauch gestattet.
+        Soweit die Inhalte auf dieser Seite nicht vom Betreiber erstellt wurden,
+        werden die Urheberrechte Dritter beachtet. Insbesondere werden Inhalte
+        Dritter als solche gekennzeichnet. Sollten Sie trotzdem auf eine
+        Urheberrechtsverletzung aufmerksam werden, bitten wir um einen
+        entsprechenden Hinweis. Bei Bekanntwerden von Rechtsverletzungen werden
+        wir derartige Inhalte umgehend entfernen.
+      </Typography>
+      <Typography variant="body1" paragraph>
+        <strong>Datenschutz</strong> <br />
+        Die Nutzung unserer Webseite ist in der Regel ohne Angabe
+        personenbezogener Daten möglich. Soweit auf unseren Seiten
+        personenbezogene Daten (beispielsweise Name, Anschrift oder
+        E-Mail-Adressen) erhoben werden, erfolgt dies, soweit möglich, stets auf
+        freiwilliger Basis. Diese Daten werden ohne Ihre ausdrückliche
+        Zustimmung nicht an Dritte weitergegeben. Wir weisen darauf hin, dass
+        die Datenübertragung im Internet (z.B. bei der Kommunikation per E-Mail)
+        Sicherheitslücken aufweisen kann. Ein lückenloser Schutz der Daten vor
+        dem Zugriff durch Dritte ist nicht möglich. Der Nutzung von im Rahmen
+        der Impressumspflicht veröffentlichten Kontaktdaten durch Dritte zur
+        Übersendung von nicht ausdrücklich angeforderter Werbung und
+        Informationsmaterialien wird hiermit ausdrücklich widersprochen. Die
+        Betreiber der Seiten behalten sich ausdrücklich rechtliche Schritte im
+        Falle der unverlangten Zusendung von Werbeinformationen, etwa durch
+        Spam-Mails, vor.
+      </Typography>
+    </Box>
+  );
+};
+
+export default Disclaimer;
